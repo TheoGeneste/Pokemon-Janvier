eca3c317d514b916bb5fc7d9d3ad4f88147442b6
Théo <tgeneste@fcdigital.fr>
2024-03-08 12:22:36 +0100

    Mise en place du projet, création de nos pages et system de rooting

---
+++ src/Pages/GenerationsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GenerationsPage = () => {
+    // États -> States
+
+    // Comportements -> Les functions
+
+    //Affichage -> return
+    return <>
+        <h1>Générations Page</h1>
+    </>
+}
+
+export default GenerationsPage
---
+++ src/Pages/PokemonsPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PokemonsPage = () => {
+    // États -> States
+
+    // Comportements -> Les functions
+
+    //Affichage -> return
+    return <>
+        <h1>Pokemons</h1>
+    </>
+}
+
+export default PokemonsPage;
