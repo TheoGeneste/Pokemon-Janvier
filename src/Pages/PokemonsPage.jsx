aceb97c864858bfe6c418e73fecae0f75a3f071c
Théo <tgeneste@fcdigital.fr>
2024-03-25 15:41:00 +0100

    Ajout du filtre de recherche

--- src/Pages/PokemonsPage.jsx
+++ src/Pages/PokemonsPage.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import PokemonCard from "../Components/PokemonCard";
 import PokemonsService from "../Services/PokemonsService";
 import Pagination from 'react-bootstrap/Pagination';
+import { Form } from "react-bootstrap";
 
 const PokemonsPage = () => {
     // États -> States
     const [pokemons, setPokemons] = useState([]);
+    const [pokemonsFiltered, setPokemonFiltered] = useState([])
     const [pokemonPerPage, setPokemonPerPage] = useState(20);
     const [currentPage, setCurrentPage] = useState(1);
     const [maxPage, setMaxPage] = useState(20);
+    const [searchValue, setSearchValue] = useState("")
 
     // Comportements -> Les functions
     const getPokemons = async () => {
@@ -16,17 +19,36 @@ const PokemonsPage = () => {
         const response = await PokemonsService.fetchPokemon(pokemonPerPage, nombrePokemonAffiche);
         setMaxPage(Math.ceil(response.data.count / pokemonPerPage));
         setPokemons(response.data.results);
+        setPokemonFiltered(response.data.results);
+    }
+
+    const handleChange = (e) => {
+        setSearchValue(e.target.value)
     }
 
     useEffect(() => {
         getPokemons();
     }, [currentPage])
 
+    useEffect(() => {
+        if (searchValue != "") {
+            let res = pokemons.filter((pokemon) => {
+                return pokemon.name.toLowerCase().includes(searchValue.toLowerCase())
+            })
+            setPokemonFiltered(res);
+        }else{
+            setPokemonFiltered(pokemons)
+        }
+    }, [searchValue])
+
     //Affichage -> return
     return <>
         <h1 className="text-center">Pokemons</h1>
+        <div className="d-flex justify-content-center m-4 col-11">   
+            <Form.Control type="text" placeholder="Rechercher un Pokemon" onChange={handleChange}/>
+        </div>
         <div className="d-flex gap-3 flex-wrap justify-content-center">
-            {pokemons.map((pokemon) => {
+            {pokemonsFiltered.map((pokemon) => {
                 return <PokemonCard nom={pokemon.name} id={pokemon.url.slice(33).replaceAll("/", "")} />
             })}
         </div>
